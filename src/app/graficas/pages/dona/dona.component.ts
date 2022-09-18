d2079e6a86758e449d4432df1eac9c701cd408bf
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-09-18 16:42:15 -0300

    Grafica de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DonaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DonaComponent {
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#69F5A1', '#75F9FA', '#80AAE0'],
+      },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 }
