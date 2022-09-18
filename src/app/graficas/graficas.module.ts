430d35a32c0aefb54b0339d83726770f0513adc3
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-09-18 15:28:51 -0300

    Grafica de barra

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgChartsModule } from 'ng2-charts';
+
 import { GraficasRoutingModule } from './graficas-routing.module';
+
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
 import { DonaComponent } from './pages/dona/dona.component';
 import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
-
 
 @NgModule({
   declarations: [
@@ -15,11 +17,8 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
     BarrasDobleComponent,
     DonaComponent,
     DonaHttpComponent,
-    GraficaBarraComponent
+    GraficaBarraComponent,
   ],
-  imports: [
-    CommonModule,
-    GraficasRoutingModule
-  ]
+  imports: [CommonModule, GraficasRoutingModule, NgChartsModule],
 })
-export class GraficasModule { }
+export class GraficasModule {}
